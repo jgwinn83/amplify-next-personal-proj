5c59cc0c62766cae3e007499d022540d425177de
Justin Gwinn <gwinnj3@gmail.com>
2025-04-16 20:17:37 -0500

    added auth

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,7 @@ export default function App() {
   return (
     <main>
       <h1>My todos</h1>
+      
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
---
+++ app/signin/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+//import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(outputs);
+
+export default function SignIn() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
