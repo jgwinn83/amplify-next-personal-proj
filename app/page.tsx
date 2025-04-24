f96ec54c28061d0161575511abfd766c45d7d697
Justin Gwinn <gwinnj3@gmail.com>
2025-04-23 20:27:36 -0500

    made some blank pages a messed with layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,55 +7,40 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import RootLayout from "./layout";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+type City = {
+  id: number;
+  name: string;
+  discription: string;
+  country: string;
+  population: number;
+  image: string;
+  isCapital: boolean;
+}
+
+type Country = {
+  id: number;
+  name: string;
+  discription: string;
+  capital: City;
+  population: number;
+  image: string;
+}
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-    
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+  
+  
 
   return (
     <main>
-      <h1>My todos</h1>
       
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      
+      
+      
     </main>
   );
 }
